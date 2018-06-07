0b2be1b3a3ce8a5af027fcd10b7fd2a4642b67ba
physikier <dani.arnold@gmx.de>
2018-06-07 15:08:05 +0200

    add features to device form

--- frontend/src/DeviceForm.jsx
+++ frontend/src/DeviceForm.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import { InputProps } from '@material-ui/core/Input'
 // import { withStyles } from '@material-ui/core/styles';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
@@ -15,17 +15,6 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 
-const styles = theme => ({
-  root: {
-    display: 'flex',
-  },
-  formControl: {
-    margin: theme.spacing.unit * 3,
-  },
-  group: {
-    margin: `${theme.spacing.unit}px 0`,
-  },
-});
 class DeviceForm extends Component {
 
   state = {
@@ -48,6 +37,11 @@ class DeviceForm extends Component {
       linkedDevice,
     } = device;
 
+    const radioOnChangeHandler = (event) => {
+      const value = event.target.value;
+      console.log(value);
+    };
+
     return (
       <div>
         <Card raised>
@@ -64,54 +58,52 @@ class DeviceForm extends Component {
             <Typography variant="title" component="h5">
               linked device: {(linkedDevice && linkedDevice.name) || 'No linked device'}
             </Typography>
-            <TextField type="number" 
-            label="current level" 
-            //defaultValue="0" 
-            InputProps={{inputProps: { min:  "0", max: "100", step: this.state.stepSize } }}
-            value={this.state.currLevel}
-            onChange ={(e) => {this.setState({ currLevel: e.target.value })}}
+            <TextField type="number"
+              label="current level"
+              //defaultValue="0" 
+              InputProps={{ inputProps: { min: "0", max: "100", step: this.state.stepSize } }}
+              value={this.state.currLevel}
+              onChange={(e) => { this.setState({ currLevel: e.target.value }) }}
             >
             </TextField>
-            <TextField type="number" 
-            label="step" 
-            defaultValue="1" 
-            InputProps={{inputProps: { min:  "0", max: "12", step: "1" }  }}
-            //value={this.state.stepSize}
-            onChange = {(e)  => {
-              this.setState({ stepSize: e.target.value || 1 });
-              console.log(e);
-            }}
-            > 
+            <TextField type="number"
+              label="step"
+              defaultValue="1"
+              InputProps={{ inputProps: { min: "0", max: "12", step: "1" } }}
+              //value={this.state.stepSize}
+              onChange={(e) => {
+                this.setState({ stepSize: e.target.value || 1 });
+                console.log(e);
+              }}
+            >
             </TextField>
-              <FormControl component="fieldset">
-                <FormLabel component="legend">
+            <FormControl component="fieldset">
+              <FormLabel component="legend">
                 unit
                 </FormLabel>
-                <RadioGroup
+              <RadioGroup
                 aria-label="unit"
-                style={{display: 'flex', flexDirection: 'row'}}
+                style={{ display: 'flex', flexDirection: 'row' }}
                 value={this.state.unit}
-                onChange={(e) => {
-                  this.setState({unit: e.target.value});
-                }}
-                >
+                onChange={radioOnChangeHandler}
+              >
                 <FormControlLabel
                   value="mA"
-                  control={<Radio color="primary"/>}
+                  control={<Radio color="primary" />}
                   label="mA"
                 />
                 <FormControlLabel
                   value="uA"
-                  control={<Radio color="primary"/>}
+                  control={<Radio color="primary" />}
                   label="uA"
                 />
                 <FormControlLabel
                   value="nA"
-                  control={<Radio color="primary"/>}
+                  control={<Radio color="primary" />}
                   label="nA"
                 />
-                </RadioGroup>
-              </FormControl>
+              </RadioGroup>
+            </FormControl>
           </CardContent>
           <CardActions>
             <Button size="small" onClick={() => connect(address)}>Connect</Button>
